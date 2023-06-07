1d8dc16e781592f1fac7ae8a1dc11661f3e7b6ab
malaw-hmrc <malawarayan.ganesan@digital.hmrc.gov.uk>
2023-06-07 14:58:05 +0100

    feat(CTCP-3227): add tests

--- test/api/AuthorisationsSpec.scala
+++ test/api/AuthorisationsSpec.scala
@@ -144,6 +144,217 @@ class AuthorisationsSpec extends SpecBase {
         converted shouldBe expected
       }
 
+      "Authorization Type should be ACE when ProcedureType is Simplified and should convert to API format" in {
+
+        val json: JsValue = Json.parse(s"""
+             |{
+             |  "_id" : "c8fdf8a7-1c77-4d25-991d-2a0881e05062",
+             |  "mrn" : "$mrn",
+             |  "eoriNumber" : "GB1234567",
+             |  "data" : {
+             |    "identification" : {
+             |      "destinationOffice" : {
+             |        "id" : "GB000051",
+             |        "name" : "Felixstowe",
+             |        "phoneNumber" : "+44 (0)1394 303023 / 24 / 26"
+             |      },
+             |      "identificationNumber" : "GB123456789000",
+             |      "isSimplifiedProcedure" : "simple",
+             |      "authorisations" : [
+             |        {
+             |          "typeValue" : "SSE",
+             |          "referenceNumber" : "SSE1"
+             |        },
+             |        {
+             |          "typeValue" : "ACR",
+             |          "referenceNumber" : "ACR1"
+             |        }
+             |      ]
+             |    },
+             |    "locationOfGoods" : {
+             |      "typeOfLocation" : "authorisedPlace",
+             |      "qualifierOfIdentification" : "customsOffice",
+             |      "qualifierOfIdentificationDetails" : {
+             |        "customsOffice" : {
+             |          "id" : "GB000142",
+             |          "name" : "Belfast EPU",
+             |          "phoneNumber" : "+44 (0)3000 523068"
+             |        }
+             |      }
+             |    },
+             |    "incidentFlag" : true,
+             |    "incidents" : [
+             |      {
+             |        "incidentCountry" : {
+             |          "code" : "GB",
+             |          "description" : "United Kingdom"
+             |        },
+             |        "incidentCode" : "partiallyOrFullyUnloaded",
+             |        "incidentText" : "foo",
+             |        "addEndorsement" : true,
+             |        "endorsement" : {
+             |          "date" : "2023-01-01",
+             |          "authority" : "bar",
+             |          "country" : {
+             |            "code" : "GB",
+             |            "description" : "United Kingdom"
+             |          },
+             |          "location" : "foobar"
+             |        },
+             |        "qualifierOfIdentification" : "unlocode",
+             |        "unLocode" : {
+             |          "unLocodeExtendedCode" : "ADCAN",
+             |          "name" : "Canillo"
+             |        },
+             |        "equipments" : [
+             |          {
+             |            "containerIdentificationNumberYesNo" : true,
+             |            "containerIdentificationNumber" : "1",
+             |            "addSealsYesNo" : true,
+             |            "seals" : [
+             |              {
+             |                "sealIdentificationNumber" : "1"
+             |              }
+             |            ],
+             |            "addGoodsItemNumberYesNo" : true,
+             |            "itemNumbers" : [
+             |              {
+             |                "itemNumber" : "1"
+             |              }
+             |            ]
+             |          }
+             |        ]
+             |      }
+             |    ]
+             |  },
+             |  "lastUpdated" : {
+             |    "$$date" : {
+             |      "$$numberLong" : "1662546803472"
+             |    }
+             |  },
+             |  "createdAt" : {
+             |    "$$date" : {
+             |      "$$numberLong" : "1662546803472"
+             |    }
+             |  }
+             |}
+             |""".stripMargin)
+
+        val uA: UserAnswers = json.as[UserAnswers](UserAnswers.mongoFormat)
+
+        val converted = Authorisations.transform(uA)
+
+        val expected = Seq(
+          AuthorisationType01(
+            sequenceNumber = "1",
+            typeValue = "ACE",
+            referenceNumber = "SSE1"
+          ),
+          AuthorisationType01(
+            sequenceNumber = "2",
+            typeValue = "ACE",
+            referenceNumber = "ACR1"
+          )
+        )
+
+        converted shouldBe expected
+      }
+
+      "Authorization Type should Not be set when ProcedureType is Normal and should convert to API format" in {
+
+        val json: JsValue = Json.parse(s"""
+             |{
+             |  "_id" : "c8fdf8a7-1c77-4d25-991d-2a0881e05062",
+             |  "mrn" : "$mrn",
+             |  "eoriNumber" : "GB1234567",
+             |  "data" : {
+             |    "identification" : {
+             |      "destinationOffice" : {
+             |        "id" : "GB000051",
+             |        "name" : "Felixstowe",
+             |        "phoneNumber" : "+44 (0)1394 303023 / 24 / 26"
+             |      },
+             |      "identificationNumber" : "GB123456789000",
+             |      "isSimplifiedProcedure" : "normal"
+             |    },
+             |    "locationOfGoods" : {
+             |      "typeOfLocation" : "authorisedPlace",
+             |      "qualifierOfIdentification" : "customsOffice",
+             |      "qualifierOfIdentificationDetails" : {
+             |        "customsOffice" : {
+             |          "id" : "GB000142",
+             |          "name" : "Belfast EPU",
+             |          "phoneNumber" : "+44 (0)3000 523068"
+             |        }
+             |      }
+             |    },
+             |    "incidentFlag" : true,
+             |    "incidents" : [
+             |      {
+             |        "incidentCountry" : {
+             |          "code" : "GB",
+             |          "description" : "United Kingdom"
+             |        },
+             |        "incidentCode" : "partiallyOrFullyUnloaded",
+             |        "incidentText" : "foo",
+             |        "addEndorsement" : true,
+             |        "endorsement" : {
+             |          "date" : "2023-01-01",
+             |          "authority" : "bar",
+             |          "country" : {
+             |            "code" : "GB",
+             |            "description" : "United Kingdom"
+             |          },
+             |          "location" : "foobar"
+             |        },
+             |        "qualifierOfIdentification" : "unlocode",
+             |        "unLocode" : {
+             |          "unLocodeExtendedCode" : "ADCAN",
+             |          "name" : "Canillo"
+             |        },
+             |        "equipments" : [
+             |          {
+             |            "containerIdentificationNumberYesNo" : true,
+             |            "containerIdentificationNumber" : "1",
+             |            "addSealsYesNo" : true,
+             |            "seals" : [
+             |              {
+             |                "sealIdentificationNumber" : "1"
+             |              }
+             |            ],
+             |            "addGoodsItemNumberYesNo" : true,
+             |            "itemNumbers" : [
+             |              {
+             |                "itemNumber" : "1"
+             |              }
+             |            ]
+             |          }
+             |        ]
+             |      }
+             |    ]
+             |  },
+             |  "lastUpdated" : {
+             |    "$$date" : {
+             |      "$$numberLong" : "1662546803472"
+             |    }
+             |  },
+             |  "createdAt" : {
+             |    "$$date" : {
+             |      "$$numberLong" : "1662546803472"
+             |    }
+             |  }
+             |}
+             |""".stripMargin)
+
+        val uA: UserAnswers = json.as[UserAnswers](UserAnswers.mongoFormat)
+
+        val converted = Authorisations.transform(uA)
+
+        val expected = List.empty
+
+        converted shouldBe expected
+      }
+
     }
   }
 }
