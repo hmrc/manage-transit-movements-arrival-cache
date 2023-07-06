4a4000468e916b07b0e24b6b4a95917a75b57798
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2023-07-06 13:15:09 +0100

    CTCP-3482: Setting message recipient value from office of destination.

--- test/api/HeaderSpec.scala
+++ test/api/HeaderSpec.scala
@@ -19,6 +19,8 @@ package api
 import api.submission.Header
 import base.SpecBase
 import generated.{CORRELATION_IDENTIFIERSequence, MESSAGE_1Sequence, MESSAGE_FROM_TRADERSequence}
+import models.UserAnswers
+import play.api.libs.json.{JsValue, Json}
 
 class HeaderSpec extends SpecBase {
 
@@ -26,14 +28,44 @@ class HeaderSpec extends SpecBase {
 
     "message is called" should {
 
+      val json: JsValue = Json.parse(s"""
+          |{
+          |  "_id" : "$uuid",
+          |  "mrn" : "$mrn",
+          |  "eoriNumber" : "$eoriNumber",
+          |  "isSubmitted" : false,
+          |  "data" : {
+          |    "identification" : {
+          |      "destinationOffice" : {
+          |        "id" : "GB000142",
+          |        "name" : "Belfast EPU",
+          |        "phoneNumber" : "+44 (0)3000 523068"
+          |      }
+          |    }
+          |  },
+          |  "createdAt" : {
+          |    "$$date" : {
+          |      "$$numberLong" : "1662393524188"
+          |    }
+          |  },
+          |  "lastUpdated" : {
+          |    "$$date" : {
+          |      "$$numberLong" : "1662546803472"
+          |    }
+          |  }
+          |}
+          |""".stripMargin)
+
+      val uA: UserAnswers = json.as[UserAnswers](UserAnswers.mongoFormat)
+
       "convert to API format" in {
 
-        val converted = Header.message
+        val converted = Header.message(uA)
 
         val expected = MESSAGE_FROM_TRADERSequence(
           messageSender = Some("NCTS"),
           messagE_1Sequence2 = MESSAGE_1Sequence(
-            messageRecipient = "NCTS",
+            messageRecipient = "NTA.GB",
             preparationDateAndTime = converted.messagE_1Sequence2.preparationDateAndTime,
             messageIdentification = "CC007C"
           )
