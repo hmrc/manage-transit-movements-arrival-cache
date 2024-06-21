7fe54abc59c62379865a9ef17d482c119f0df8ad
Reece-Carruthers <99188015+Reece-Carruthers@users.noreply.github.com>
2024-06-21 10:29:03 +0100

    Update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.3.0"
-  private val hmrcMongoVersion = "1.6.0"
+  private val bootstrapVersion = "9.0.0"
+  private val hmrcMongoVersion = "2.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "uk.gov.hmrc"             %% "crypto-json-play-30"        % "7.6.0",
+    "uk.gov.hmrc"             %% "crypto-json-play-30"        % "8.0.0",
     "javax.xml.bind"           % "jaxb-api"                   % "2.3.1",
-    "org.typelevel"           %% "cats-core"                  % "2.10.0"
+    "org.typelevel"           %% "cats-core"                  % "2.12.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
-    "org.scalatest"           %% "scalatest"                  % "3.2.15",
+    "org.scalatest"           %% "scalatest"                  % "3.2.18",
     "org.scalatestplus"       %% "mockito-4-6"                % "3.2.15.0",
-    "org.scalacheck"          %% "scalacheck"                 % "1.17.0",
-    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.15.0",
+    "org.scalacheck"          %% "scalacheck"                 % "1.18.0",
+    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0",
   ).map(_ % "test")
 }
