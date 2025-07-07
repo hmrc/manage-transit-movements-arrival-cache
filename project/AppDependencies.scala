7ca5b68b0b10b8eefd6fec04c30d1e90d9a6b98e
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2025-07-07 14:58:04 +0100

    CTCP-6571: Dependency updates.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.14.0"
+  private val mongoVersion     = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % mongoVersion,
     "uk.gov.hmrc"             %% "crypto-json-play-30"        % "8.2.0",
     "javax.xml.bind"           % "jaxb-api"                   % "2.3.1",
     "org.typelevel"           %% "cats-core"                  % "2.13.0"
@@ -15,10 +15,10 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % mongoVersion,
     "org.scalatest"           %% "scalatest"                  % "3.2.19",
     "org.scalatestplus"       %% "mockito-4-6"                % "3.2.15.0",
     "org.scalacheck"          %% "scalacheck"                 % "1.18.1",
-    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0",
+    "org.scalatestplus"       %% "scalacheck-1-17"            % "3.2.18.0"
   ).map(_ % "test")
 }
